fd66ccaad8936b48839266fdd6c23988117dafff
Odorizzi1 <88588959+Odorizzi1@users.noreply.github.com>
2023-11-05 23:30:23 -0300

    create project and configs

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+
+import { useState } from 'react'
+import * as S from './styles'
+
+const Main = () => {
+
+  return (
+    <>
+      teste123
+    </>
+  )
+}
+export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,11 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: black;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  display: flex;
+`
